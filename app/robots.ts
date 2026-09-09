6d3d4bd60b8eab336253f2dadb79213ea377218c
harmonicaccord7 <harmonicaccord7@gmail.com>
2026-09-09 16:50:06 +0200

    seo: exclude private and transactional surfaces from crawling

--- app/robots.ts
+++ app/robots.ts
@@ -2,5 +2,9 @@ import type { MetadataRoute } from 'next'
 
 export default function robots():MetadataRoute.Robots{
  const base='https://www.kaporalintelligence.com'
- return {rules:[{userAgent:'*',allow:'/',disallow:['/studio','/api/studio','/auth']}],sitemap:`${base}/sitemap.xml`,host:base}
+ return {
+  rules:[{userAgent:'*',allow:'/',disallow:['/studio','/api','/auth','/account','/newsletter/confirm','/newsletter/unsubscribe']}],
+  sitemap:`${base}/sitemap.xml`,
+  host:base
+ }
 }
